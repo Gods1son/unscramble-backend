f314550a585b8972e7691e42aa8149d9c78f539c
gods1son <tolulopeibiyode@gmail.com>
2017-06-01 21:59:10 -0400

    new app

---
+++ server.js
@@ -0,0 +1,53 @@
+  var express = require('express');
+  var app = express();
+var port = process.env.PORT || 7777;
+  var server = require('http').createServer(app);
+  var io = require('socket.io')(server);
+
+server.listen(port);
+/*io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+});*/
+
+  app.get('/', function(req, res, next) {
+  	res.sendFile(__dirname + '/public/index.html')
+  });
+
+  app.use(express.static('public'));
+
+ var usernames = {};
+
+io.sockets.on('connection', function (socket) {
+
+    // when the client emits 'sendchat', this listens and executes
+    socket.on('sendchat', function (data) {
+        // we tell the client to execute 'updatechat' with 2 parameters
+        io.sockets.emit('updatechat', socket.username, data);
+    });
+
+    // when the client emits 'adduser', this listens and executes
+    socket.on('adduser', function(username){
+        // we store the username in the socket session for this client
+        socket.username = username;
+        // add the client's username to the global list
+        usernames[username] = username;
+        // echo to client they've connected
+        socket.emit('updatechat', 'SERVER', 'you have connected');
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+        // update the list of users in chat, client-side
+        io.sockets.emit('updateusers', usernames);
+    });
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', function(){
+        // remove the username from global usernames list
+        delete usernames[socket.username];
+        // update list of users in chat, client-side
+        io.sockets.emit('updateusers', usernames);
+        // echo globally that this client has left
+        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+    });
+});
+
