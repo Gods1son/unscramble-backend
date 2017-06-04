1a6f454b759dc8bf89ba0126f22f30ddf856cb07
Gods1son <tolulopeibiyode@gmail.com>
2017-06-04 01:36:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ io.sockets.on('connection', function (socket) {
 		usernamesList[socket.room] = "";
 		usernamesList[socket.room] += socket.username + "<br>";
 		io.sockets.in(socket.room).emit('allUsers', usernamesList[socket.room]);		
-		io.sockets.in(socket.room).emit('updateScores',socket.username, socket.scores);
+		io.sockets.in(socket.room).emit('startScores',socket.username, socket.scores);
 		}else{socket.emit('roomCreatedError',roomName + ' has already been chosen')}
 	})
 
@@ -94,7 +94,7 @@ io.sockets.on('connection', function (socket) {
 		//socket.allUsers.push(socket.username);
 		usernamesList[socket.room] += socket.username + "<br>";
 		io.sockets.in(socket.room).emit('allUsers', usernamesList[socket.room]);
-		io.sockets.in(socket.room).emit('updateScores',socket.username, socket.scores);
+		io.sockets.in(socket.room).emit('startScores',socket.username, socket.scores);
 		//io.sockets.in(socket.room).emit('updateScores',socket.username + " correct" + " = " + socket.scores["correct"],socket.username + " incorrect" + " = " + socket.scores["incorrect"]);
 	})
 	
