89c8a489f765af8ed7ad29689c9ced5d36800df8
Gods1son <tolulopeibiyode@gmail.com>
2017-07-11 16:21:42 -0400

    Update server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ io.sockets.on('connection', function (socket) {
 		var n = d.toLocaleTimeString();
 		var theword = newWord;
 		var player = "i just played";
-		socket.emit('newWord3',socket.username + "'s QUESTION -- " + newWord + " " + n, socket.room, player);
+		socket.emit('newWord3',socket.username + "'s QUESTION is -- ",newWord, socket.room, player);
 		socket.broadcast.to(socket.room).emit('newWord',theword, socket.username + "'s QUESTION is -- " , socket.room, originalWord);
 		//socket.broadcast.to(roomName2).emit('newWord', newWord);
 	})
