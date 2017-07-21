ca8f9f0c4e076fe1fca356b47be57ee3f715219a
Gods1son <tolulopeibiyode@gmail.com>
2017-07-21 19:07:01 -0400

    Update client.js

--- public/client.js
+++ public/client.js
@@ -5,7 +5,9 @@ var dictionary = new Typo("en_US", false, false, { dictionaryPath: "typo/diction
  // on connection to server, ask for user's name with an anonymous callback
   socket.on('connect', function(){
 		// call the server-side function 'adduser' and send one parameter (value of prompt)
-		socket.emit("pickUsername",prompt("Pick a username"));
+		var user = prompt("Pick a username");
+	  	document.getElementById("userNameIs").innerHTML = "Welcome " + user;
+		socket.emit("pickUsername", user);
 	});
 socket.on('disconnectedUser', function(info){
 	alert(info);
