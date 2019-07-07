373c2d2a2c5bcab99687cc83a85aca41f251d4d0
Gods1son <tolulopeibiyode@gmail.com>
2019-07-07 18:38:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/db', async (req, res) => {
       const client = await pool.connect()
       const result = await client.query('SELECT * FROM users');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('public/db', results );
+      res.render('public/db.html', results );
       client.release();
     } catch (err) {
       console.error(err);
