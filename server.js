4c52bf6c4111b64da3a5a053b365b300d8bd9eac
Gods1son <tolulopeibiyode@gmail.com>
2019-07-07 18:57:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,23 +14,17 @@ server.listen(port);
   	res.sendFile(__dirname + '/public/index.html')
   });
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
-app.get('/db', async (req, res) => {
+
+/*app.get('/db', async (req, res) => {
     try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM users');
-      const results = { 'results': (result) ? result.rows : null};
+      
       res.render('public/db', results );
-      client.release();
+      
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
     }
-  })
+  })*/
 
 app.use(express.static('public'));
 
@@ -40,30 +34,20 @@ var usernamesList = [];
 var rooms = ['room1','room2','room3'];
 var roomName2;
 io.sockets.on('connection', function (socket) {
-		
-	//socket.allUsers = [];
-	// when the client emits 'adduser', this listens and executes
-	/*socket.on('adduser', function(username){
-		// store the username in the socket session for this client
-		socket.username = username;
-		// store the room name in the socket session for this client
-		socket.room = 'room1';
-		// add the client's username to the global list
-		usernames[username] = username;
-		// send client to room 1
-		socket.join('room1');
-		// echo to client they've connected
-		socket.emit('updatechat', 'SERVER', 'you have connected to room1');
-		// echo to room 1 that a person has connected to their room
-		socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
-		socket.emit('updaterooms', rooms, 'room1');
-	});*/
 	//creating userName
 	socket.on('pickUsername', function (username) {
 		socket.username = username;
 		socket.scores = 0;
-		
-		socket.emit('welcomeHere', username);		
+		const { Pool } = require('pg');
+		const pool = new Pool({
+		  connectionString: process.env.DATABASE_URL,
+		  ssl: true
+		});
+		const client = await pool.connect()
+      		const result = await client.query('SELECT * FROM users');
+      		const results = { 'results': (result) ? result.rows : null};
+		client.release();
+		socket.emit('welcomeHere', username, results);		
 	});
 	// when the client emits 'sendchat', this listens and executes
 	socket.on('sendchat', function (data) {
