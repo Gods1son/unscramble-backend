604fc55473e530bf206996d6fd62ac2078699a14
Gods1son <tolulopeibiyode@gmail.com>
2019-07-29 20:53:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -32,10 +32,10 @@ app.use(function (req, res, next) {
 
  app.get('/', function(req, res, next) {
 	//res.render('index'); 
-	 res.setHeader('Access-Control-Allow-Origin', '*');
+	 /*res.setHeader('Access-Control-Allow-Origin', '*');
     	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
     	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-    	res.setHeader('Access-Control-Allow-Credentials', true);
+    	res.setHeader('Access-Control-Allow-Credentials', true);*/
 	 res.render('index'); 
  })
 
