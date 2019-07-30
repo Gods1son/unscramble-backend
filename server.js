49cc6af76df6969856cc546d6fd0f55880ea2f65
Gods1son <tolulopeibiyode@gmail.com>
2019-07-29 21:00:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+   /* res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -27,7 +27,7 @@ app.use(function (req, res, next) {
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-	next();
+	next();*/
 });
 
  app.get('/', function(req, res, next) {
